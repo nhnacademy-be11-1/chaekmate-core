04fe69604996f30deef35689160978c3d49248d5
chzhxkd100 <chzhxkd100@naver.com>
2025-10-22 12:21:31 +0900

    feat: Tag CRUD 구현

--- src/main/java/shop/chaekmate/core/book/entity/Tag.java
+++ src/main/java/shop/chaekmate/core/book/entity/Tag.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 import shop.chaekmate.core.common.entity.BaseEntity;
@@ -26,6 +27,11 @@ public class Tag extends BaseEntity {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
+    @Setter
     @Column(nullable = false)
     private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
 }
