0987c3af9b47fc56611edee0817b26137ee8608e
chzhxkd100 <chzhxkd100@naver.com>
2025-10-23 10:57:13 +0900

    feat: swagger jwt 토큰 입력 추가

---
+++ src/main/java/shop/chaekmate/core/config/OpenApiSecurityConfig.java
@@ -0,0 +1,31 @@
+package shop.chaekmate.core.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+// Swagger UI 활용 시 JWT 토큰 입력을 위한 Config
+@Configuration
+public class OpenApiSecurityConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .name("BearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+
+    }
+}
