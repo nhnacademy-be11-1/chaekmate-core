0eb9c247cd777c82b7f4b84553f1218a19c9b007
chzhxkd100 <chzhxkd100@naver.com>
2025-10-26 15:15:24 +0900

    refactor: bookImage 분리

--- src/main/java/shop/chaekmate/core/book/entity/BookImage.java
+++ src/main/java/shop/chaekmate/core/book/entity/BookImage.java
@@ -5,6 +5,7 @@ import static lombok.AccessLevel.PROTECTED;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
