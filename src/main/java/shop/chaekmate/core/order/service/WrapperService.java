f9ef9fd8858d436f7b2686e3df0d79546caf5f3a
Gwon <121155771+shingigwon@users.noreply.github.com>
2025-10-27 11:18:22 +0900

    Feat/wrapper기능 구현(test포함) 및 swagger추가, Profile설정 추가 (#5)
    * workflows yml 파일 설정
    
    * fix:entity수정
    
    * fix:wrapper entity uk제거
    
    * feat:wrapper 기능 구현
    
    * fix:existByName 수정
    
    * fix:existByName 수정(되돌림)
    
    * feat:wrapper기능(test추가) swagger추가
    
    * refactor:DB 접속 정보를 환경변수로 분리
    
    * fix:수정사항반영
    
    * fix:entity수정
    
    * fix:wrapper entity uk제거
    
    * feat:wrapper 기능 구현
    
    * fix:existByName 수정
    
    * fix:existByName 수정(되돌림)
    
    * feat:wrapper기능(test추가) swagger추가
    
    * refactor:DB 접속 정보를 환경변수로 분리
    
    * fix:수정사항반영
    
    ---------
    
    Co-authored-by: charles <kch1759@naver.com>
    Co-authored-by: JeongHun Yu <jeomxon@gmail.com>
    Co-authored-by: chzhxkd100 <42908189+chzhxkd100@users.noreply.github.com>

---
+++ src/main/java/shop/chaekmate/core/order/service/WrapperService.java
@@ -0,0 +1,72 @@
+package shop.chaekmate.core.order.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.chaekmate.core.order.dto.request.WrapperDto;
+import shop.chaekmate.core.order.dto.response.WrapperResponse;
+import shop.chaekmate.core.order.entity.Wrapper;
+import shop.chaekmate.core.order.exception.DuplicatedWrapperNameException;
+import shop.chaekmate.core.order.exception.WrapperNotFoundException;
+import shop.chaekmate.core.order.repository.WrapperRepository;
+
+@Service
+@RequiredArgsConstructor
+public class WrapperService {
+    private final WrapperRepository wrapperRepository;
+
+    //포장지 이름, 가격 추가
+    @Transactional
+    public WrapperResponse createWrapper(WrapperDto wrapperDto) {
+        if (wrapperRepository.existsByName(wrapperDto.name())) {
+            throw new DuplicatedWrapperNameException(wrapperDto.name());
+        }
+        Wrapper wrapper = wrapperRepository.save(new Wrapper(wrapperDto.name(), wrapperDto.price()));
+
+        return new WrapperResponse(wrapper.getId(), wrapper.getName(), wrapper.getPrice());
+    }
+
+    //포장지 수정(이름+가격)
+    @Transactional
+    public WrapperResponse modifyWrapper(Long id, WrapperDto wrapperDto) {
+        Wrapper wrapper = wrapperRepository.findById(id).orElseThrow(() -> new WrapperNotFoundException(id));
+
+        if (!wrapper.equalsName(wrapperDto.name())
+                && wrapperRepository.existsByName(wrapperDto.name())) {
+            throw new DuplicatedWrapperNameException(wrapperDto.name());
+        }
+
+        wrapper.updateWrapper(wrapperDto.name(), wrapperDto.price());
+
+        return new WrapperResponse(wrapper.getId(), wrapper.getName(), wrapper.getPrice());
+    }
+
+    //포장지 삭제
+    @Transactional
+    public void deleteWrapper(Long id) {
+        if (!wrapperRepository.existsById(id)) {
+            throw new WrapperNotFoundException(id);
+        }
+
+        wrapperRepository.deleteById(id);
+    }
+
+    //포장지 단일 조회
+    @Transactional(readOnly = true)
+    public WrapperResponse getWrapperById(Long id) {
+        Wrapper wrapper = wrapperRepository.findById(id).orElseThrow(() -> new WrapperNotFoundException(id));
+        return new WrapperResponse(wrapper.getId(), wrapper.getName(), wrapper.getPrice());
+    }
+
+    //포장지 전체 조회
+    @Transactional(readOnly = true)
+    public List<WrapperResponse> getWrappers() {
+        return wrapperRepository.findAll().stream()
+                .map(wrapper -> new WrapperResponse(
+                        wrapper.getId(),
+                        wrapper.getName(),
+                        wrapper.getPrice()))
+                .toList();
+    }
+}
