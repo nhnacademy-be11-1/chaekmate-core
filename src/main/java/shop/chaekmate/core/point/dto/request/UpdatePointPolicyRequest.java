77a27acbd320a788b200757ec7aefb20924e6d4e
AH100-1 <qkrtkddn6815@gmail.com>
2025-11-04 16:16:20 +0900

    fix: 포인트 Entity 어노테이션 추가

--- src/main/java/shop/chaekmate/core/point/dto/request/UpdatePointPolicyRequest.java
+++ src/main/java/shop/chaekmate/core/point/dto/request/UpdatePointPolicyRequest.java
@@ -2,13 +2,14 @@ package shop.chaekmate.core.point.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import shop.chaekmate.core.point.entity.type.PointEarnedType;
 
 public record UpdatePointPolicyRequest(
         @NotNull(message = "적용할 정책 타입은 필수입니다.")
         PointEarnedType type,
         @NotNull(message = "포인트 값은 필수입니다.")
-        @Min(value = 0, message = "포인트 값은 0 이상이어야 합니다")
+        @PositiveOrZero
         Integer point
 ) {
 }
