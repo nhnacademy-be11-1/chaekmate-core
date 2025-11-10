9ee69f4e1e7c0c09e0bb124e25973357121ae935
chzhxkd100 <42908189+chzhxkd100@users.noreply.github.com>
2025-11-10 10:34:32 +0900

    feat: 관리자 최근 추가된 도서 조회 기능 (#29)
    * feat: 도서CRUD 구현
    
    * feat: BookImage 생성자, updateUrl메서드 생성
    
    * feat: 최근 추가된 도서 조회
    
    * feat: SwaggerConfig 추가
    
    * feat: Test 에서 메소드명 한글 사용 경고 제거
    
    * fix: 책 이미지를 찾을 수 없을때 null을 반환하도록 수정
    
    * fix: 도서 업데이트 시 도서 이미지가 없을 때도 추가할 수 있게 수정
    
    * fix: 도서 업데이트 시 도서 이미지가 없을 때도 추가할 수 있게 수정
    
    * feat: Controller 테스트코드 작성
    
    * feat: swagger 문서화 추가
    
    * feat: 충돌 수정
    
    * fix: 이름변경
    
    ---------
    
    Co-authored-by: charles <kch1759@naver.com>

---
+++ src/test/java/shop/chaekmate/core/book/controller/AdminBookControllerTest.java
@@ -0,0 +1,44 @@
+package shop.chaekmate.core.book.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+import shop.chaekmate.core.book.entity.Book;
+import shop.chaekmate.core.book.repository.BookRepository;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class AdminBookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    void 최근_추가된_도서_조회_요청_성공() throws Exception {
+        bookRepository.save(
+                new Book("test title", "testIndex", "testDesc", "testAuthor", "testPublisher", LocalDateTime.now(),
+                        "1234567891123", 100, 100, false, 0, false, 0));
+
+        mockMvc.perform(get("/admin/books/recent?limit=1"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.[0].title").value("test title"));
+    }
+}
