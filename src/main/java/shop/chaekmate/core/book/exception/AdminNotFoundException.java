9cd72d4dbf6c920f471be975be294f83bcc0a12a
chzhxkd100 <chzhxkd100@naver.com>
2025-11-11 15:57:46 +0900

    fix: codeSmell제거

--- src/main/java/shop/chaekmate/core/book/exception/AdminNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package shop.chaekmate.core.book.exception;
-
-import shop.chaekmate.core.common.exception.CoreException;
-
-public class AdminNotFoundException extends CoreException {
-    public AdminNotFoundException() {
-        super(BookErrorCode.ADMIN_NOT_FOUND);
-    }
-}
