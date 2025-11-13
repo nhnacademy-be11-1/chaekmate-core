5b3b1641c1c6ff22ad8c364083f155d7b93d47c1
AH100-1 <qkrtkddn6815@gmail.com>
2025-11-13 14:03:52 +0900

    fix: Transaction (readOnly = false) 추가, Swagger config 변경

--- src/main/java/shop/chaekmate/core/point/exception/MemberNotFoundException.java
+++ src/main/java/shop/chaekmate/core/point/exception/MemberNotFoundException.java
@@ -1,9 +1,10 @@
 package shop.chaekmate.core.point.exception;
 
+import shop.chaekmate.core.book.exception.LikeErrorCode;
 import shop.chaekmate.core.common.exception.CoreException;
 
 public class MemberNotFoundException extends CoreException {
     public MemberNotFoundException() {
-        super(PointErrorCode.MEMBER_NOT_FOUND);
+        super(LikeErrorCode.MEMBER_NOT_FOUND);
     }
 }
