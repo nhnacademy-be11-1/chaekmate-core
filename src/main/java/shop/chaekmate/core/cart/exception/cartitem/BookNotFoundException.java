c0ee2aa4ea0fdaa5a5fe4f33bf079260bd05aeca
chaewonn <scw0629@naver.com>
2025-11-14 13:23:41 +0900

    fix: 장바구니 구현 초기화 (#49)

--- src/main/java/shop/chaekmate/core/cart/exception/cartitem/BookNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package shop.chaekmate.core.cart.exception.cartitem;
-
-import shop.chaekmate.core.common.exception.CoreException;
-
-public class BookNotFoundException extends CoreException {
-    public BookNotFoundException() {
-        super(CartItemErrorCode.BOOK_NOT_FOUND);
-    }
-}
--- src/main/java/shop/chaekmate/core/cart/exception/cartitem/CartItemInvalidQuantityException.java
+++
@@ -1,9 +0,0 @@
-package shop.chaekmate.core.cart.exception.cartitem;
-
-import shop.chaekmate.core.common.exception.CoreException;
-
-public class CartItemInvalidQuantityException extends CoreException {
-  public CartItemInvalidQuantityException() {
-    super(CartItemErrorCode.CART_ITEM_INVALID_QUANTITY);
-  }
-}
