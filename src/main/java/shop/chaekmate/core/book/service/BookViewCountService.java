c36d505c2b95991b19ff31461d8c16ab065bba2a
chzhxkd100 <chzhxkd100@naver.com>
2025-11-17 16:39:47 +0900

    feat: TTL 추가

--- src/main/java/shop/chaekmate/core/book/service/BookViewCountService.java
+++ src/main/java/shop/chaekmate/core/book/service/BookViewCountService.java
@@ -1,5 +1,6 @@
 package shop.chaekmate.core.book.service;
 
+import java.time.Duration;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -20,6 +21,10 @@ public class BookViewCountService {
 
         String key = PREFIX + bookId;
 
+        // 먼저 키를 0으로 초기화하고 TTL 5분 지정 (실제론 1분마다 DB반영하면서 삭제)
+        redisTemplate.opsForValue().setIfAbsent(key, "0", Duration.ofMinutes(5));
+
+        // 증가
         redisTemplate.opsForValue().increment(key);
     }
 
