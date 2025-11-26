0ac936ab8d4efa8f3b3ff1d2e9e7c751a48f3d8d
chaewonn <scw0629@naver.com>
2025-11-26 09:57:56 +0900

    refactor: 장바구니 담기/수량변경 시 도서 재고 검증 로직 추가 (#84)
    Co-authored-by: chzhxkd100 <chzhxkd100@naver.com>

--- src/main/java/shop/chaekmate/core/cart/exception/CartErrorCode.java
+++ src/main/java/shop/chaekmate/core/cart/exception/CartErrorCode.java
@@ -8,7 +8,7 @@ import shop.chaekmate.core.common.exception.BaseErrorCode;
 public enum CartErrorCode implements BaseErrorCode {
 
     CART_NOT_FOUND(HttpStatus.NOT_FOUND, "CART-404", "해당 장바구니를 찾을 수 없습니다."),
-    CART_INVALID_OWNER(HttpStatus.BAD_REQUEST, "CART-404", "장바구니 소유자가 유효하지 않습니다."),
+    BOOK_INSUFFICIENT_STOCK(HttpStatus.BAD_REQUEST, "CART-ITEM-400", "해당 도서의 재고가 부족합니다."),
     CART_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "CART-ITEM-404", "해당 장바구니 아이템을 찾을 수 없습니다.");
 
     private final HttpStatus status;
